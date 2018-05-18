b7c66363703a23f8122be5a41c6d85826ef2138f
[qióng] <xtjatswc@users.noreply.github.com>
2018-05-18 14:55:12 +0800

    将Areas扩展到外部类库

--- FlexibleMVC/Global.asax.cs
+++ FlexibleMVC/Global.asax.cs
@@ -9,29 +9,57 @@ using System.Web.Routing;
 
 namespace FlexibleMVC.Web
 {
-	public class MvcApplication : HttpApplication
-	{
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.Ignore("{resource}.axd/{*pathInfo}");
-			
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new {
-					controller = "Home",
-					action = "Index",
-					id = UrlParameter.Optional
-				},
-				namespaces : new string[]{ "FlexibleMVC.Web.Controllers" }
-			);
-		}
-		
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
+    public class MvcApplication : HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Ignore("{resource}.axd/{*pathInfo}");
 
-			RegisterRoutes(RouteTable.Routes);
-		}
-	}
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                },
+                namespaces: new string[] { "FlexibleMVC.Web.Controllers" }
+            );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //自定义视图引擎，如改名默认视图路径
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new MvcViewEngine());
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+
+    public class MvcViewEngine : RazorViewEngine
+    {
+        public MvcViewEngine()
+        {
+            MasterLocationFormats = new[] 
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shard/{0}.cshtml",
+            };
+            ViewLocationFormats = MasterLocationFormats;
+            PartialViewLocationFormats = MasterLocationFormats;
+
+            AreaMasterLocationFormats = new[] {
+                "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/FlexibleMVC.Web.Admin/Areas/{2}/Views/{1}/{0}.cshtml",
+                "~/FlexibleMVC.Web.Admin/Areas/{2}/Views/Shared/{0}.cshtml",
+            };
+            AreaViewLocationFormats = AreaMasterLocationFormats;
+            AreaPartialViewLocationFormats = AreaMasterLocationFormats;
+
+        }
+    }
 }
