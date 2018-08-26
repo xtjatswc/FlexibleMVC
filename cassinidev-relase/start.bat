b19c59091e5e8b0c1a7a6f92286480480d433ef5
xtjatswc <565032163@qq.com>
2018-08-26 14:42:15 +0800

    启动程序后自动关闭dos窗口，并打开网页

--- cassinidev-relase/start.bat
+++ cassinidev-relase/start.bat
@@ -1,1 +1,2 @@
-CassiniDev4.exe /ip:0.0.0.0 /im:Any /pm:Specific /port:32778 /path:"../FlexibleMVC"
+start CassiniDev4.exe /ip:0.0.0.0 /im:Any /pm:Specific /port:32778 /path:"../FlexibleMVC"
+start http://localhost:32778
