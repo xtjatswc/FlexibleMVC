f25df13536aeb694df0ce46247cc2a22fd479a93
[qióng] <xtjatswc@users.noreply.github.com>
2018-11-14 18:38:46 +0800

    1

--- FlexibleMVC/FlexibleMVC.Web.Bjdc/Areas/System/Controllers/MealOrderController.cs
+++ FlexibleMVC/FlexibleMVC.Web.Bjdc/Areas/System/Controllers/MealOrderController.cs
@@ -3,6 +3,7 @@ using FlexibleMVC.LessBase.Ctrller;
 using FlexibleMVC.LessBase.Filters.Permission;
 using FlexibleMVC.Web.Bjdc.Areas.System.DAL;
 using System.Web.Mvc;
+using FlexibleMVC.LessBase.Extension;
 
 namespace FlexibleMVC.Web.Bjdc.Areas.System.Controllers
 {
@@ -21,8 +22,8 @@ namespace FlexibleMVC.Web.Bjdc.Areas.System.Controllers
         public JsonResult GetOrderList()
         {
             //查询条件
-            //string SaleName = Request.GetSqlParamer("SaleName");
-            //string CategoryID = GetString("CategoryID");
+            string ContactMobile = Request.GetSqlParamer("ContactMobile");
+            string ContactName = Request.GetSqlParamer("ContactName");
             //分页
             int pageIndex = GetInt("pageIndex") + 1;
             int pageSize = GetInt("pageSize");
@@ -30,12 +31,12 @@ namespace FlexibleMVC.Web.Bjdc.Areas.System.Controllers
             //String sortField = GetString("sortField");
             //String sortOrder = GetString("sortOrder");
 
-            //string sWhere = "CategoryID='" + CategoryID + "' and SaleName like '%" + SaleName + "%'";
+            string sWhere = "ContactMobile like '%" + ContactMobile + "%' and ContactName like '%" + ContactName + "%'";
 
             var mealMenuDal = flexibleContext.GetService<MealOrderDal>();
 
-            var models = mealMenuDal.GetModels(where: "", currentPage: pageIndex, itemsPerPage: pageSize, orderBy:"OrderID desc");
-            var count = mealMenuDal.GetCount(where: "");
+            var models = mealMenuDal.GetModels(where: sWhere, currentPage: pageIndex, itemsPerPage: pageSize, orderBy:"OrderID desc");
+            var count = mealMenuDal.GetCount(where: sWhere);
 
             var result = new { total = count, data = models };
             return Json(result);
