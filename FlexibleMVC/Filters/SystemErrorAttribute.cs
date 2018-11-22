520186533e7ff23affaf9011e3e6440643ac74b8
xtjatswc <565032163@qq.com>
2018-11-22 23:20:53 +0800

    添加404页面自定义错误页面

--- FlexibleMVC/Filters/SystemErrorAttribute.cs
+++ FlexibleMVC/Filters/SystemErrorAttribute.cs
@@ -17,20 +17,6 @@ namespace FlexibleMVC.Web.Filters
         public override void OnException(ExceptionContext filterContext)
         {
             base.OnException(filterContext);
-            //处理错误消息，将其跳转到一个页面
-            //string controllerName = (string)filterContext.RouteData.Values["controller"];
-            //string actionName = (string)filterContext.RouteData.Values["action"];
-
-            //这里简单向C盘的test.log写入了文件
-            //FileStream fs = new FileStream(@"e:\test.log", FileMode.OpenOrCreate, FileAccess.Write);
-            //StreamWriter sw = new StreamWriter(fs);
-            //sw.BaseStream.Seek(0, SeekOrigin.End);
-            //string writeText = string.Format("controllerName:[{0}]actionName:[{1}]{2}",
-            //    controllerName, actionName, filterContext.Exception.ToString());
-            //sw.WriteLine(writeText);
-            //sw.Flush();
-            //sw.Close();
-            //fs.Close();
             
             //这里是使用log4net来记录
             log4net.ILog log = log4net.LogManager.GetLogger(filterContext.Controller.GetType());
