37fd86ca36117b6f04f7d7db250b8795f3ea80dc
[qióng] <xtjatswc@users.noreply.github.com>
2018-12-04 12:58:40 +0800

    using 结束后,事务的标志位仍然为 true ,如果之后还有更新追加操作,由于没有手动Commit,会导致数据丢失; 现修改为 using 结束时,自动重置标志位为 false ,之后的数据操作不再会自动开启新事务,也就没有漏提交的问题了.

--- Base/FluentData/Context/DbContext.cs
+++ Base/FluentData/Context/DbContext.cs
@@ -27,6 +27,11 @@
 		public void Dispose()
 		{
 			CloseSharedConnection();
-		}
+            /*
+             * using 结束后,事务的标志位仍然为 true ,如果之后还有更新追加操作,由于没有手动Commit,会导致数据丢失;
+             * 现修改为 using 结束时,自动重置标志位为 false ,之后的数据操作不再会自动开启新事务,也就没有漏提交的问题了.
+             */
+            Data.UseTransaction = false;
+        }
 	}
 }
